d2a9b7dfca0cfe79c5cd47b72b1dc726fc2d43fb
alya016 <60424440+alya016@users.noreply.github.com>
2021-10-09 14:39:40 +0600

    hw4

--- hw4openplease/time/time.go
+++
@@ -1,5 +0,0 @@
-package time
-
-func MealSchedules(calories int) []*MealByHour {
-	// (тут можно использовать time)
-}
--- hw4openplease/time/time_test..go
+++
@@ -1,6 +0,0 @@
-package time
-func MealSchedules(t *testing.T){
-	a,b := 1,2
-	calories := CaloriesByMeal(a, b)
-	assert.Equal(t, 222, calories)
-}
