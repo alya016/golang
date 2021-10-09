8607625f768f0453e0114664517b71fa56e67b42
alya016 <60424440+alya016@users.noreply.github.com>
2021-10-09 21:09:00 +0600

    updTesting

--- hw4openplease/stringt/stringt_test.go
+++ hw4openplease/stringt/stringt_test.go
@@ -1,1 +1,20 @@
 package stringt
+
+import(
+	"homework/facility"
+	"testing"
+)
+
+func TestCheckExcistenceFacility(t *testing.T){
+	facility1 := facility.Facility{
+		Town: "Shu",
+		Coordinates: []float64 {2.00,3.00,8.00,9.00,},
+		SalaryToGuard: 10000.00,
+		PaymentFromClient: 12000.00,
+	}
+	expectedIsExist := true
+	actualIsExist := CheckExcistenceFacility(facility1.Town)
+	if expectedIsExist != actualIsExist{
+		t.Fail()
+	}
+}
